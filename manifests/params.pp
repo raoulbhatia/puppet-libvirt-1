d9723beb5511e9a6e01198ff553aa6dae8be6075
Raoul Bhatia <raoul@bhatia.at>
2017-11-03 10:20:32 +0100

    Add support for Ubuntu 17.10/Artful

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,6 +13,10 @@ class libvirt::params {
           $libvirt_package_names  = ['libvirt-daemon-system', 'qemu']
           $service_name           = 'libvirtd'
         }
+        'artful': {
+          $libvirt_package_names  = ['libvirt-bin']
+          $service_name           = 'libvirtd'
+        }
         default: {
           fail("${::lsbdistcodename} is currently not supported by the libvirt
                 module. Please add support for it and submit a patch!")
